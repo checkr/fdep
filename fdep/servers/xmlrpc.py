ac7935011443d1f95078e09eb36a1a28e8ca2d90
Stewart Park <hello@stewartjpark.com>
2017-01-27 10:17:58 -0800

    Implement basic XMLRPC server

--- fdep/servers/xmlrpc.py
+++ fdep/servers/xmlrpc.py
@@ -1,11 +1,20 @@
 from fdep.servers import RPCServer
 
+try:
+    from SimpleXMLRPCServer import SimpleXMLRPCServer
+except ImportError:
+    from xmlrpc.server import SimpleXMLRPCServer
+
 
 class XMLRPCServer(RPCServer):
     """Implement a XMLRPC server."""
 
     def register_functions(self, func_pairs):
-        pass
+        self.func_pairs = func_pairs
 
     def serve_forever(self, **kwargs):
-        pass
+        server = SimpleXMLRPCServer(('0.0.0.0', kwargs['port']))
+
+        for name, func in self.func_pairs:
+            server.register_function(func, name=name)
+        server.serve_forever()
